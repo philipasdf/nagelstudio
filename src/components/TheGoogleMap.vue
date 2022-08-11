e8add9f6a6932d81930d8f8f3a8ff1c6ec8c4a33
philipasdf <ng.philip.91@gmail.com>
2022-08-11 07:59:54 +0200

    add google maps view

---
+++ src/components/TheGoogleMap.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="mapouter" :style="{ height: mapDimension, width: mapDimension }">
+    <div class="gmap_canvas" :style="{ height: mapDimension, width: mapDimension }">
+      <iframe
+        :height="mapDimension"
+        :width="mapDimension"
+        id="gmap_canvas"
+        src="https://maps.google.com/maps?q=Steinweg%2020%20Coburg&t=&z=17&ie=UTF8&iwloc=&output=embed"
+        frameborder="0"
+        scrolling="no"
+        marginheight="0"
+        marginwidth="0"
+      ></iframe
+      ><br />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const maxDimension = 650;
+let mapDimension = Math.min(window.innerWidth, maxDimension);
+</script>
+<style scoped>
+.mapouter {
+  position: relative;
+  text-align: right;
+}
+.gmap_canvas {
+  overflow: hidden;
+  background: none !important;
+}
+</style>
--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -1,7 +1,9 @@
 <template>
-  <Preisliste></Preisliste>
+  <Preisliste />
+  <TheGoogleMap />
 </template>
 
 <script setup lang="ts">
 import Preisliste from '../components/preisliste/Preisliste.vue';
+import TheGoogleMap from '../components/TheGoogleMap.vue';
 </script>
