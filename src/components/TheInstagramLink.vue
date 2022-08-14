875570b5e6bb4cbc6479d403a5311d7fbbf1cdf1
philipasdf <ng.philip.91@gmail.com>
2022-08-14 18:13:26 +0200

    add instagram link

---
+++ src/components/TheInstagramLink.vue
@@ -0,0 +1,20 @@
+<template>
+  <a href="http://www.instagram.com/queennailscoburg/" target="blank">
+    <img src="@/assets/icons/Instagram_logo_2016.svg" alt="SVG mit img laden" width="35" height="35" />
+    <span>queennailscoburg</span>
+  </a>
+</template>
+
+<script setup lang="ts"></script>
+<style scoped>
+a {
+  margin: 1rem;
+}
+img,
+span {
+  vertical-align: middle;
+}
+span {
+  margin: 1rem;
+}
+</style>
--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -1,9 +1,11 @@
 <template>
   <Preisliste />
   <TheGoogleMap />
+  <TheInstagramLink />
 </template>
 
 <script setup lang="ts">
 import Preisliste from '../components/preisliste/Preisliste.vue';
 import TheGoogleMap from '../components/TheGoogleMap.vue';
+import TheInstagramLink from '../components/TheInstagramLink.vue';
 </script>
