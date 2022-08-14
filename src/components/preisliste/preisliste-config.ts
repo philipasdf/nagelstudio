d47335d22a46b3ff233f7c52cfc729a4436c9869
philipasdf <ng.philip.91@gmail.com>
2022-08-14 21:39:41 +0200

    update preisliste

--- src/components/preisliste/preisliste-config.ts
+++ src/components/preisliste/preisliste-config.ts
@@ -57,6 +57,78 @@ export const PREISE: PreisAbschnitt[] = [
       },
     ],
   },
+  {
+    name: 'FRENCH',
+    items: [
+      {
+        beschreibung: 'Basic',
+        preis: '5',
+      },
+      {
+        beschreibung: 'mit Lack',
+        preis: '5',
+      },
+      {
+        beschreibung: 'Glitzer',
+        preis: '10',
+      },
+      {
+        beschreibung: 'French oder Farbe',
+        preis: '37',
+      },
+    ],
+  },
+  {
+    name: 'FÜSSE',
+    items: [
+      {
+        beschreibung: 'Pediküre',
+        preis: '25',
+      },
+      {
+        beschreibung: 'mit Lack',
+        preis: '30',
+      },
+      {
+        beschreibung: 'French oder Farbe',
+        preis: '40',
+      },
+    ],
+  },
+  {
+    name: 'NEUMODELLAGE',
+    items: [
+      {
+        beschreibung: 'mit Acryl',
+        preis: '40',
+      },
+      {
+        beschreibung: 'mit French Pulver',
+        preis: '40',
+      },
+      {
+        beschreibung: 'Shellac',
+        preis: '25',
+      },
+    ],
+  },
+  {
+    name: 'PFLEGE',
+    items: [
+      {
+        beschreibung: 'Kürzen und Feilen',
+        preis: '7',
+      },
+      {
+        beschreibung: 'Reparatur',
+        preis: '5',
+      },
+      {
+        beschreibung: 'Versiegeln mit extra Glanz',
+        preis: '6',
+      },
+    ],
+  },
 ];
 
 export interface PreisAbschnitt {
